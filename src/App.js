730cf989486751e151188b2e33ea5284e0937c70
Davidasahi <maple11303@gmail.com>
2023-11-07 16:49:36 -0500

    Set up the environment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+
+// import Box from './Components/Box';
+// import Row from './Components/Row';
+
 const App = () => {
-  return <h1>Hello React </h1>;
+  return;
+
+  // <div id="board">
+  <h1>Hello React </h1>;
+  // </div>;
 };
 
 export default App;
