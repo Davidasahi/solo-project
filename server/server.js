af4f60584cca4690da2621e64a9c2601bf2ccda9
Davidasahi <maple11303@gmail.com>
2023-11-08 17:33:26 -0500

    I can see the dogs

--- server/server.js
+++ server/server.js
@@ -6,17 +6,17 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('../captcha/:index', (req, res) => {
-  // const index = req.params.index;
-  // console.log('What is this: ', { index });
+app.get('/captcha/:index', (req, res) => {
+  console.log('can you see me?');
+  const index = req.params.index;
+  const imagePath = path.resolve(__dirname, `../src/photos/dog${index}.png`);
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      // Handle file sending error
+      res.status(404).send('File not found');
+    }
+  });
 
-  res.sendFile(path.resolve(__dirname, `../src/photos/dog${index}.png`));
-
-  // const imageBuffer = fs.readFileSync(
-  //   path.resolve(__dirname, `../src/photos/dog${index}.png`)
-  // );
-
-  // const imageBuffer = fs.readFileSync(`../src/photos/dog${index}.png`);
   // res.setHeader('Content-Type', 'image/png');
   // res.sendFile(imageBuffer);
 });
