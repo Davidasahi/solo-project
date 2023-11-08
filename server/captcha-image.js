85d059b3869d0b4032152f1d2a3cd374a65adfc2
Davidasahi <maple11303@gmail.com>
2023-11-08 12:31:27 -0500

    Done till noon

--- server/captcha-image.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const fileController = require('../controllers/fileController');
-const router = express.Router();
--- src/index.js
+++ src/index.js
@@ -4,27 +4,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.scss';
 
-// require routers
-const apiRouter = require('./routes/api.js');
-
-//handle requests for static files
-// app.use(express.static('client'));
-
-//define router handler
-// app.use('/api', apiRouter);
-
-// route handler to respond with main app
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
-});
-
-// catch-all route handler for any requests to an unknown route
-app.all('*', (req, res) => {
-  console.log('Error send the message');
-  res.sendStatus(404);
-});
-
 // const el = document.getElementById('app');
 // ReactDOM.render(<App />, el);
 
-module.exports = app;
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
