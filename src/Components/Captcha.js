ebfbcbc13cc8c6c9475754c74ba6c696a28e306d
Davidasahi <maple11303@gmail.com>
2023-11-08 11:20:58 -0500

    Check 01

--- src/Components/Captcha.js
+++ src/Components/Captcha.js
@@ -1,25 +1,35 @@
 import React from 'react';
 
-const imageLocation = new Array(9).fill(null).map((value, index) => {
-  return `/src/photos/image.jpeg`;
+const imageLocations = new Array(9).fill(null).map((value, index) => {
+  return `/src/photos/image01.jpeg`;
 });
 
-console.log('This is testing: ', imageLocation);
+// src/photos/image01.jpeg
+// ${index}
+console.log('This is testing: ', imageLocations);
 // const imageLocation = (new Array(arrayLength: 9)).fill(value:null).map((value, index: number) => {
 
 const captcha = () => {
+  const imageElements = imageLocations.map((imageUrl) => {
+    return (
+      <div>
+        <img src={imageUrl} alt="" />
+      </div>
+    );
+  });
+
+  console.log();
   return (
     <div className="Captcha">
       <h2>Select the dogs</h2>
-      <div className="Captcha-image">
-        {imageLocation.map((imageUrls) => {
-          <div>
-            <img src="imageUrls" alt="" />;
-          </div>;
-        })}
-      </div>
+      <div className="Captcha-image">{imageElements}</div>
     </div>
   );
 };
 
 export default captcha;
+
+// .map((imageUrls) => {
+//     <div>
+//       <img src="imageUrls" alt="" />
+//     </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.scss';
 
-const el = document.getElementById('app');
+// const el = document.getElementById('app');
+// ReactDOM.render(<App />, el);
 
-ReactDOM.render(<App />, el);
+const el = document.getElementById('app');
+const root = createRoot(el); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
