246caa5060421d4cd856c3197c5defac17bbb466
Davidasahi <maple11303@gmail.com>
2023-11-09 12:05:15 -0500

    Finish click the dog photo to be transparent

--- src/Components/Captcha.js
+++ src/Components/Captcha.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+// Array containing paths to 9 images
 const imageLocations = new Array(9).fill(null).map((value, index) => {
   return `/src/photos/dog${index}.png`;
 });
 
-const captcha = ({ key }) => {
-  //Shulle the images
-  const shuffledImages = imageLocations.sort(() => Math.random() - 0.5);
+const Captcha = ({ key }) => {
+  // State to hold the shuffled images
+  const [shuffledImages, setShuffledImages] = useState([]);
 
-  //Render the image on 9 boxes on index.html
+  // Effect to shuffle images when the key changes
+  useEffect(() => {
+    // Shuffle the images when the key changes
+    setShuffledImages(() => imageLocations.sort(() => Math.random() - 0.5));
+  }, [key]);
+
+  // State to keep track of selected images
+  const [selectedImages, setSelectedImages] = useState(new Set());
+
+  // Function to handle image click
+  const handleImageClick = (index) => {
+    const updatedSet = new Set(selectedImages);
+    // Toggle selection: add if not selected, delete if already selected
+    if (updatedSet.has(index)) {
+      updatedSet.delete(index);
+    } else {
+      updatedSet.add(index);
+    }
+    setSelectedImages(updatedSet);
+  };
+
+  // Map through shuffled images to create elements
   const imageElements = shuffledImages.map((imageUrl, index) => {
+    const isSelected = selectedImages.has(index);
     return (
-      <div key={index}>
-        <img src={imageUrl} alt="" />
+      <div key={index} onClick={() => handleImageClick(index)}>
+        {/* Render images with opacity based on selection */}
+        <img src={imageUrl} alt="" style={{ opacity: isSelected ? 0.5 : 1 }} />
       </div>
     );
   });
 
-  //return on index.html
   return (
     <div>
       <h2>Select the dogs</h2>
@@ -26,4 +49,4 @@ const captcha = ({ key }) => {
   );
 };
 
-export default captcha;
+export default Captcha;
