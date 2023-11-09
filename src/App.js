00b64f44669337fdf7aada95f2af4b413da7fb70
Davidasahi <maple11303@gmail.com>
2023-11-09 12:15:26 -0500

    Empty the name field after I click the button.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Captcha from './Components/Captcha.js';
 
 const App = () => {
   const [key, setKey] = useState(0);
+  const setRef = useRef(null); // Create a reference to the input element
 
   //Shuffle button function click
   function handleClick() {
     setKey((prevKey) => prevKey + 1);
   }
 
+  // Reset the input field value to an empty string
+  if (setRef.current) {
+    setRef.current.value = '';
+  }
+
   //return on index.html
   return (
     <div class="main">
       <h1>Captcha Doggy challenge</h1>
-      <input type="test" id="name" placeholder="Your name"></input>
+      <input type="test" id="name" ref={setRef} placeholder="Your name"></input>
       <div>
         <Captcha key={key} />
       </div>
       <button id="button" onClick={handleClick}>
         Shuffle!
       </button>
-      <button id="button">Send your answer!</button>
+      <button id="button">More challenge</button>
     </div>
   );
 };
